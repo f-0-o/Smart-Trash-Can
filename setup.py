d5327edad8edee0756e7eb96d412a7295c3e055d
f-0-o <98905164+f-0-o@users.noreply.github.com>
2022-03-20 23:03:29 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name ="Smart Trash Can",
+      version ="Alpha 1.0",
+      description ="Packages to be installed: Distance sensor, OLED etc",
+      author ="Fahim Ahmed", "Amolak Plaha",
+      py_modules=["ds"],["text_display"],
+     )
