8787393dfd2421392924b3cad353abe71eea08bc
f-0-o <98905164+f-0-o@users.noreply.github.com>
2022-04-06 13:08:25 -0400

    Add files via upload

---
+++ foo.py
@@ -0,0 +1,37 @@
+
+class trashcan:
+
+	def __init__(self):
+		self.display = display
+
+	def image_display(self):
+		#TODO
+		return
+
+	def send_text(self):
+		#TODO
+		return
+
+	def get_device(self):
+		#TODO
+		return
+
+	def display_setting(self):
+		#TODO
+		return
+
+	def measure_distance(self):
+		#TODO
+		return
+
+	def button_left(self):
+		#TODO
+		return
+
+	def button_middle(self):
+		#TODO
+		return
+
+	def button_right(self):
+		#TODO
+		return
