b9ec9f11d753bc117d19e0f63c0f094416ca1516
f-0-o <98905164+f-0-o@users.noreply.github.com>
2022-04-06 13:47:25 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,5 +4,6 @@ setup(name ="Smart Trash Can",
       version ="Alpha 1.0",
       description ="Packages to be installed: Distance sensor, OLED etc",
       author ="Fahim Ahmed", "Amolak Plaha",
+      packages = luma.core(), pathlib(), PIL(), 
       py_modules=["ds"],["text_display"],
      )
